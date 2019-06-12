ac2937264025a8a270c9fceaa06f7c63fbd61cdb
Bernardo Albuquerque <marambaia.bma@gmail.com>
2019-06-12 00:31:44 -0300

    Atualiza versão do jQuery

--- mysql.php
+++ mysql.php
@@ -56,7 +56,7 @@
         </div>
       </div>
     </div>
-    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="./node_modules/jquery/dist/jquery.slim.min.js"></script>
     <script src="./node_modules/popper.js/dist/popper.min.js"></script>
     <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   </body>

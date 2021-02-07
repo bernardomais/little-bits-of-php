0c321f861bc1e0d1237645865b682f00f93efc52
Bernardo Albuquerque <marambaia.bma@gmail.com>
2021-02-07 00:40:04 -0300

    Security updates in dependencies

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <title>PHP Code list</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="text-center">PHP Code list:</h3>
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-md-10">
+            
+        </div>
+      </div>
+    </div>
+    <script src="./node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="./node_modules/popper.js/dist/popper.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
